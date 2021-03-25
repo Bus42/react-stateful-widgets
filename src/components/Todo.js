4b41e677c710696210bb7f85bc8454b2a8fc78e3
Greg Brewton <gbrewton1980@gmail.com>
2021-03-25 00:37:27 -0600

    Adds comments

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,3 +1,8 @@
+// It may look a little different than some solutions because I did not use a tutorial. 
+// Instead, I thought of how I would approach the solution from the ground up with existing React knowledge.
+// I wanted to show *my* solution to a problem.
+// I would not normally just throw all the files in the same folder, but this was a simple exercise.
+
 import React, { useState } from "react";
 import TodoForm from "./TodoForm";
 import TodoItem from "./TodoItem";
@@ -5,17 +10,21 @@ import TodoItem from "./TodoItem";
 const Todo = () => {
   const [Todos, setTodos] = useState([]);
 
+  // use id to sort tasks and remove the target task
   const markComplete = (id) => {
-    console.log(id)
     const newList = [...Todos];
     const newNewList = newList.filter(task => task.id != id)
     console.log(newNewList);
     setTodos(newNewList);
   };
+  // creates a new task with title, content, and id supplied by TodoForm component
   const addTodo = (title, content, id) => {
     const newItem = { title, content, id };
+    // creates identical array instead of acting on state object
     const newList = [...Todos];
+    // add new task to list
     newList.push(newItem);
+    // update state
     setTodos(newList);
   };
   return (
